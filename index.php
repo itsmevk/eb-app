c02760ae60c89cf833bdb167249662f9a6d133e8
itsmevk <Vishwajeet.11906790@lpu.in>
2023-10-08 02:23:56 +0530

    second

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 
 <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="#">Learning Management System</a>
+            <a class="navbar-brand">Learning Management System</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
