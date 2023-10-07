628a7a5ad234284552ebc2c508c0bb0684fd7f0d
itsmevk <Vishwajeet.11906790@lpu.in>
2023-10-08 02:17:51 +0530

    first test commit

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Information - ABC School</title>
+    <title>Student Information - LMS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+mv3s5Pq2vL+umfuHR3fSNmKfPPI8jF8Jw6GsoLniyOJbDh" crossorigin="anonymous">
     <style>
         /* Custom CSS for styling */
         body {
@@ -58,7 +59,7 @@
 
 <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="#">ABC University</a>
+            <a class="navbar-brand" href="#">Learning Management System</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
